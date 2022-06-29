aa41004b41ce23d993df8eda93bcfb2b0958ddfe
JonathasGit <jonathas-25@hotmail.com>
2022-06-28 23:12:34 -0300

    #1 PathVariable Aula 6

--- spring/src/main/java/br/com/spring/spring/Controller/InicioController.java
+++ spring/src/main/java/br/com/spring/spring/Controller/InicioController.java
@@ -1,6 +1,7 @@
 package br.com.spring.spring.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Aqui serã o as criação das rotas
@@ -11,6 +12,14 @@ public class InicioController {
     @GetMapping("")
     public String mensagem(){
         return "Hello World";
+    }
+    @GetMapping("/bemvindo")
+    public String bemVindo(){
+        return "Seja bem vindo(a)";
+    }
+    @GetMapping("/bemvindo/{nome}")
+    public String bemVindo2(@PathVariable String nome){
+        return "Seja bem vindo(a) " + nome + " a nossa Api do curso de Spring Boot";
     }
 
 
