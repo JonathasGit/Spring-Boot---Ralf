7ed7b0e39c08e7d585685d3f910a9b621a24e01b
JonathasGit <jonathas-25@hotmail.com>
2022-06-30 23:54:42 -0300

    Aula de núemro 7 - Criando modelos

---
+++ spring/src/main/java/br/com/spring/spring/model/Pessoa.java
@@ -0,0 +1,27 @@
+package br.com.spring.spring.model;
+
+public class Pessoa {
+
+
+    //Manter  atributos sempre privados
+    private String nome;
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+  
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+   
+
+    
+}
